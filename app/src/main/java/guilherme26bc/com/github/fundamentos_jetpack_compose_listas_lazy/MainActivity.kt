f319facd38f909dbb7e2d75842d9f03e37052419
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-15 21:53:32 -0300

    exibicao dos games utilizando um card

--- app/src/main/java/guilherme26bc/com/github/fundamentos_jetpack_compose_listas_lazy/MainActivity.kt
+++ app/src/main/java/guilherme26bc/com/github/fundamentos_jetpack_compose_listas_lazy/MainActivity.kt
@@ -72,6 +72,7 @@ fun GamesScreen(modifier: Modifier = Modifier) {
                 Column() {
                     Text(text = it.title)
                 }
+                GameCard(game = it)
             }
         }
     }
